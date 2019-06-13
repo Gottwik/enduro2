49d528da3b13074abe4cbd905482415ac04dcba2
Martin Gottweis <gottwik@gmail.com>
2019-06-13 15:21:07 +0200

    removed stylus

--- libs/build_tools/css_handler.js
+++ libs/build_tools/css_handler.js
@@ -9,7 +9,6 @@ css_handler.prototype.init = function (gulp, browser_sync) {
 	// stores task name
 	const css_handler_task_name = 'css'
 	const sass_handler = require(enduro.enduro_path + '/libs/build_tools/sass_handler').init(gulp, browser_sync)
-	const stylus_handler = require(enduro.enduro_path + '/libs/build_tools/stylus_handler').init(gulp, browser_sync)
 
 	gulp.task(css_handler_task_name, function() {
 		gulp.start(sass_handler)
--- scaffolding/test_stylus/app/markdown_rules/test_markdown_rule.js
+++
@@ -1,3 +0,0 @@
-module.exports = function (input) {
-	return input.replace(new RegExp(/\[(.+?)\]{(.+?)}/g), '<a class="kispander-link" href="#ksp/$2">$1</a>')
-}
